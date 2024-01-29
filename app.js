b71b0b44c0d9bd8527fefd05a0b49692e99ac5ed
Hrid <Hrid1402@gmail.com>
2024-01-29 12:24:35 -0500

    html, css and style finished

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 console.log("working...");
 const container = document.querySelector("#container");
-let numberOfGrinds = ""
+
 const changeSizeBtn = document.querySelector("#changeSize");
+const cleanButton = document.querySelector("#Clean");
+
+const colors = ["skyblue","red", "green", "blue", "violet", "yellow", "indigo", "orange", "purple"];
+let randomColors = false;
+
+const rainbowBTN = document.querySelector("#rainbow");
+
+const pbBTN = document.querySelector("#pb");
+let progressiveDark = false;
+
+const normalBTN = document.querySelector("#normal");
+function normalDrawing(){
+    progressiveDark = false;
+    randomColors = false;
+}
+
 function createGrid(numberOfGrinds) {
     for (let i = 0; i < numberOfGrinds; i++){
         const column = document.createElement("div");
@@ -10,10 +26,34 @@ function createGrid(numberOfGrinds) {
         for (let i = 0; i < numberOfGrinds; i++){
             const grid = document.createElement("div");
             grid.classList.add("grid");
+            grid.style.backgroundColor = "rgb(255, 255, 255)";
             grid.style.height = ((725/numberOfGrinds)-2).toString() + "px";
             grid.style.width = ((725/numberOfGrinds)-2).toString() + "px";
-            grid.addEventListener("mouseover", (event) => {
-                grid.style.backgroundColor = "black";
+            grid.addEventListener("mouseover", () => {
+                console.log(progressiveDark + " status pd");
+                console.log(randomColors + " rainbow");
+                console.log("actualcolor: " + grid.style.backgroundColor);
+                if(progressiveDark){
+                    if(colors.includes(grid.style.backgroundColor)){
+                        console.log("---------------------");
+                    }
+                    else{
+                        let darkPorcentage = 25.5;
+                        let text = grid.style.backgroundColor;
+                        console.log(text);
+                        let newColor = text.match(/\d+/g)[0] - darkPorcentage;
+                        grid.style.backgroundColor = "rgb(" + newColor + "," + newColor + "," + newColor + ")";
+                    }
+                    
+                }
+                else if (grid.style.backgroundColor == "rgb(255, 255, 255)"){
+                    if(randomColors){
+                        grid.style.backgroundColor = colors[(Math.floor(Math.random() * colors.length))];
+                    }
+                    else{
+                        grid.style.backgroundColor = "rgb(0,0,0)";
+                    }
+                }
             });
             column.appendChild(grid);
             } 
@@ -21,15 +61,44 @@ function createGrid(numberOfGrinds) {
         } 
 }
 createGrid(10);
-changeSizeBtn.addEventListener("click", (event) => {
+changeSizeBtn.addEventListener("click", () => {
     allColumns = document.querySelectorAll(".column");
     allColumns.forEach(element => {
         element.remove();
     });
-
-    createGrid(prompt("Please enter number of grinds for side: "));
-
+    let numberOfGrinds = prompt("Please enter number of grinds for side: ");
+    while (+numberOfGrinds <= 0 || +numberOfGrinds > 100){
+        alert("Max range is 1 - 100");
+        numberOfGrinds = prompt("Please enter number of grinds for side: ");
+    }
+    createGrid(numberOfGrinds);
 });
+
+pbBTN.addEventListener("click", () => {
+    console.log("pb pressed");
+    progressiveDark = true;
+    randomColors = false;
+});
+
+cleanButton.addEventListener("click", () => {
+    normalDrawing();
+    randomColors = false;
+    allGrids = document.querySelectorAll(".grid");
+    allGrids.forEach(element => {
+        element.style.backgroundColor = "rgb(255, 255, 255)";
+    });
+});
+
+rainbowBTN.addEventListener("click", () => {
+    console.log("rainbow pressed");
+    randomColors = true;
+    progressiveDark = false;
+});
+
+normalBTN.addEventListener("click", () => {
+    normalDrawing();
+});
+
 
 
 
